909d7d8f191a1a08b6bb62fc965c870671fe613c
koru07478-cpu <koru07478@gmail.com>
2026-02-07 07:27:40 +0300

    3.3
    Added functions to print 2x2 and 4x4 squares using a helper function.

---
+++ chapter03/ex03.py
@@ -0,0 +1,30 @@
+def print_for(strg):
+	print(strg)
+	print(strg)
+	print(strg)
+	print(strg)
+	
+def square2x2():
+	print("+ - - - - + - - - - +")
+	print_for("|         |         |")
+	print("+ - - - - + - - - - +")
+	print_for("|         |         |")
+	print("+ - - - - + - - - - +")	
+
+square2x2()
+
+
+def square4x4():
+	print("+ - - - - + - - - - + - - - - + - - - - +")
+	print_for("|         |         |         |         |")
+	print("+ - - - - + - - - - + - - - - + - - - - +")
+	print_for("|         |         |         |         |")
+	print("+ - - - - + - - - - + - - - - + - - - - +")
+	print_for("|         |         |         |         |")
+	print("+ - - - - + - - - - + - - - - + - - - - +")
+	print_for("|         |         |         |         |")
+	print("+ - - - - + - - - - + - - - - + - - - - +")
+
+square4x4()
+
+#Можно сделать больше функций, но будто это уже излишне будет
