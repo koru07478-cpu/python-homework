b6c0ecbf02ede07e94e9006e106ca026f8749f33
koru07478-cpu <koru07478@gmail.com>
2026-04-04 02:08:22 +0300

    8.2 в целом мняю само упражнение

--- chapter08/ex02.py
+++ chapter08/ex02.py
@@ -1,1 +1,22 @@
-print("банан".count("а"))
+def find(string: str, ministring: str) -> int:
+   """Возвращает индекс вхождения министроки `ministring` в строку `string`.
+
+
+   Возвращает -1, если такого вхождения нет.
+
+
+   >>> find('abc', 'b')
+   1
+   >>> find('alphabetagamma', 'beta')
+   5
+   >>> find('abc', 'd')
+   -1
+   """
+   index = 0
+   while index < len(string):
+       if string[index:(index+len(ministring))] == ministring:
+           return index
+       index = index + 1
+   return -1
+
+print(find("homak", "mak"))
