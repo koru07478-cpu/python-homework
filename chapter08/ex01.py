0f8f4b0f105a869cb18517bd3818ba8c651728ce
koru07478-cpu <koru07478@gmail.com>
2026-04-04 01:01:10 +0300

    8.1 тут сами упраженения сами поменялись, вот, переделываю теперь

--- chapter08/ex01.py
+++ chapter08/ex01.py
@@ -1,2 +1,11 @@
-a = "лол"
-print(а.strip("л")) 
+def line_in_reverse_order(string):
+    """ Принимает строку в качестве аргумента и отображает буквы в обратном порядке, по одной на строку, с помощью цикла while. """
+    index = 1
+    while index <= len(string):
+    	letter = string[-index]
+    	print(letter)
+    	index = index + 1
+    return "The end of string"
+    	 	
+
+print(line_in_reverse_order("aple"))
