935283dc1590fa7e89f609a30137e138c6c14de6
koru07478-cpu <koru07478@gmail.com>
2026-04-04 02:19:26 +0300

    8.3 с нуля

--- chapter08/ex03.py
+++ chapter08/ex03.py
@@ -1,2 +1,19 @@
-def is_palindrome(word):
-    return word == word[::-1]
+def count(string: str, ministring: str) -> int:
+   """Возвращает количество вхождений министроки `ministring` в строку `string`.
+
+   >>> find('babbcb', 'b')
+   4
+   >>> find('банан', 'а')
+   2
+   >>> find('abc', 'd')
+   -1
+   """
+   index = 0
+   count = 0
+   while index < len(string):
+       if string[index:(index+len(ministring))] == ministring:
+           count += 1
+       index = index + 1
+   return count 
+
+print(count("homakmak", "mak"))
