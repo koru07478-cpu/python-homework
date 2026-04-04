12d80b7ec414d4a8eb6d0868305e618d2a7f2b33
koru07478-cpu <koru07478@gmail.com>
2026-04-04 07:54:48 +0300

    Добавила нумерацию

--- chapter08/ex04.py
+++ chapter08/ex04.py
@@ -1,3 +1,11 @@
+#1
+#i=0 ;j=3
+#i=1 ;j=2
+#i=2 ;j=1
+#i=3 ;j=0
+#2
+#Последняя строка никогда не сравнится 
+#3
 def is_reverse(word1: str, word2: str) -> bool:
    """Проверить, являются ли word1 и word2 обращением друг друга
    >>> is_reverse('порт', 'троп')
@@ -19,8 +27,7 @@ def is_reverse(word1: str, word2: str) -> bool:
 print(is_reverse("прив","кирп")) #выводит True
 
 
-
-
+#4
 #ИСПРАВЛЕННЫЙ КОД (добавляю символ "=" в цикл while)
 def is_reverse(word1: str, word2: str) -> bool:
    """Проверить, являются ли word1 и word2 обращением друг друга
