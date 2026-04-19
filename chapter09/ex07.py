f78fb402173ca61894c885df4b513ac73cbadb59
koru07478-cpu <koru07478@gmail.com>
2026-04-19 08:56:25 +0300

    Create ex07.py

---
+++ chapter09/ex07.py
@@ -0,0 +1,13 @@
+#Ответ: bookkeeper, bookkeepers, bookkeeping, bookkeepings
+def has_three_pairs_identical_letters(word):
+    """Проверяет, наличие в слове трех последовательных удвоенных букв"""
+    for i in range(len(word) - 5):
+        if word[i] == word[i+1] and word[i+2] == word[i+3] and word[i+4] == word[i+5]:
+            return True
+    return False
+
+with open('words.txt') as f:
+    for line in f:
+        word = line.strip()
+        if has_three_pairs_identical_letters(word):
+            print(word)
