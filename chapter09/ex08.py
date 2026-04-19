cb094ef72c51eefaa945726b3d014b16bc8e00a6
koru07478-cpu <koru07478@gmail.com>
2026-04-19 08:58:05 +0300

    Create ex08.py

---
+++ chapter09/ex08.py
@@ -0,0 +1,11 @@
+#без метода zfill() получилось, не поняла, для чего он                      
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+for n in range(100000, 1000000):
+    if is_palindrome(str(n)[2:]):
+        if is_palindrome(str(n + 1)[1:]):
+            if is_palindrome(str(n + 2)[1:5]):
+                if is_palindrome(str(n + 3)):
+                    print(n)
