2559cf9412adc243b3f24342b716ad0b25e60cde
koru07478-cpu <koru07478@gmail.com>
2026-04-26 19:47:59 +0300

    исправила ошибки ex03.py

--- chapter11/ex03.py
+++ chapter11/ex03.py
@@ -1,23 +1,25 @@
 def histogram(string: str) -> dict[str, int]:
-   """
-   Считает количество каждого символа в строке string возвращает dict[str, int], где string - тип ключа, а integer - это тип значения.
-   """
-   hist = dict()
-   for char in string:
-      hist[char] = hist.get(char, 0) + 1
-   return hist
+    """
+    Считает количество каждого символа в строке string возвращает dict[str, int], где string - тип ключа, а integer - это тип значения.
+    """
+    hist = dict()
+    for char in string:
+        hist[char] = hist.get(char, 0) + 1
+    return hist
 
-def invert_dict2(d: dict[K, V]) -> dict[V, list[K]]:
-   """
-   Инвертирует словаврь, меняя значенияч на ключи, а ключи на значения.
-   >>> invert_dict2({})
-   {}
-   >>> invert_dict2({'dog': 1, 'pig': 1, 'cat': 2})
-   {1: ['dog', 'pig'], 2: ['cat']}
-   >>> invert_dict2({1/9: 1, 1.0: 1, 1+0: 2})
-   {1: [0.1111111111111111], 2: [1.0]}
-   """
-   inverse = dict()
-   for key in d:
-      inverse.setdefault("key", 0)
-   return inverse
+
+def invert_dict2(d: dict) -> dict:
+    """
+    Инвертирует словаврь, меняя значенияч на ключи, а ключи на значения.
+    >>> invert_dict2({})
+    {}
+    >>> invert_dict2({'dog': 1, 'pig': 1, 'cat': 2})
+    {1: ['dog', 'pig'], 2: ['cat']}
+    >>> invert_dict2({1/9: 1, 1.0: 1, 8: 2})
+    {1: [0.1111111111111111, 1.0], 2: [8]}
+    """
+    inverse = dict()
+    for key in d:
+        value = d[key]
+        inverse.setdefault(value, []).append(key)
+    return inverse
