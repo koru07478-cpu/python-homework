517a4c287cb777c655eb62a9a8e8f3f35e8b0d95
Sergei Shilovsky <sshilovsky@gmail.com>
2026-04-26 16:34:15 +0300

    11.4 вернул интерфейс

--- chapter11/ex04.py
+++ chapter11/ex04.py
@@ -1,14 +1,16 @@
-def has_duplicates(t: dict) -> bool:
+def has_duplicates(t: list) -> bool:
     """
     Принимает словарь и возвращает True, если есть какое-либо значение, 
     которое появляется более одного раза. Она не изменяет исходный словарь
-    >>> has_duplicates({'dog': 1, 'pig': 1, 'cat': 2})
+    >>> has_duplicates([1, 2, 1])
     True
-    >>> has_duplicates({'dog': 1, 'pig': 2, 'cat': 3})
+    >>> has_duplicates([1, 2, 3])
+    False
+    >>> has_duplicates([])
     False
     """
     hist = dict()
-    for val in t.values():
+    for val in t:
         hist[val] = hist.get(val, 0) + 1
         if hist[val] > 1:
             return True
