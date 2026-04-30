cdf4417f779e7acbf620a5cd3fbf1be352d5bd34
koru07478-cpu <koru07478@gmail.com>
2026-04-30 19:30:56 +0300

    доделала доп к 5 заданию и само задание

---
+++ chapter12/additionaltask.py
@@ -0,0 +1,13 @@
+# Условие: Дана строка из уникальных символов. Нужно вернуть список всех возможных комбинаций этих символов.
+# Пример: "abc" ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+def combined_symbols(t):
+    if len(t) <= 1: return [t]
+    res = []
+    for i in range(len(t)):
+        for j in combined_symbols(t[:i] + t[i + 1:]):
+            res.append(t[i] + j)
+    return res
+
+print(combined_symbols("abcd"))
+
+
---
+++ chapter12/ex05.py
@@ -0,0 +1,33 @@
+# Список слов в файле words.txt не содержит однобуквенных слов.
+# Поэтому  в качестве базового случая мы можем взять слова “i” и “a”.
+words = set()
+with open('words.txt', 'r') as f:
+    for line in f:
+        words.add(line.strip().lower())
+words.update(['a', 'i', ''])
+
+
+def a_letter_equals_a_new_word(word):
+    """
+    Рекурсивно ищет цепочку сокращений слова до одной буквы.
+    Возвращает список слов или [], если сокращение невозможно.
+    """
+    if word not in words:
+        return []
+    if len(word) <= 1:
+        return [word]
+
+    for i in range(len(word)):
+        res = a_letter_equals_a_new_word(word[:i] + word[i + 1:])
+        if res:
+            return [word] + res
+    return []
+
+
+all_words = sorted(list(words), key=len, reverse=True)
+
+for w in all_words:
+    path = a_letter_equals_a_new_word(w)
+    if path:
+        print(f"{w} -> {path}")
+        break
