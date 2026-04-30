3668a692dfd56df4861c4e5d772482319d1c1f61
Sergei Shilovsky <sshilovsky@gmail.com>
2026-04-30 09:31:07 +0300

    12.4 правки по стилю

--- chapter12/ex04.py
+++ chapter12/ex04.py
@@ -5,26 +5,26 @@ with open('../words.txt', 'r', encoding='utf-8') as f:
 d = {}
 for word in words:
     key = ''.join(sorted(word))
-    if key not in d:
+    if key not in d:  # setdefault
         d[key] = [word]
     else:
         d[key].append(word)
 
-for i in d.values():
+for i in d.values():  # i очень долго живёт, стоит переименовать
     if len(i) < 2:
         continue
 
     for j in range(len(i)):
         for k in range(j + 1, len(i)):
-            fer = i[j]
-            sec = i[k]
+            fer = i[j]  # можно было сделать enumerate в цикле выше
+            sec = i[k]  # а тут индекс в принципе дальше не нужен, можно было сразу букву перебирать по срезу
 
             if len(fer) != len(sec):
                 continue
 
             liist = []
             for m in range(len(fer)):
-                if fer[m] != sec[m]:
+                if fer[m] != sec[m]:  # а тут можно было через zip
                     liist.append(m)
 
             if len(liist) == 2:
