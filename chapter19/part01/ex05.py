3cda6c650df1aa9d0db93cb27535167d402363de
koru07478-cpu <koru07478@gmail.com>
2026-07-17 14:59:48 +0300

    глава 19 часть 1 в 5 добавила

--- chapter19/part01/ex05.py
+++ chapter19/part01/ex05.py
@@ -1,8 +1,9 @@
 def avoids(word: str, forbidden: str) -> bool:
-    """Возвращает True, если в слове word нет ни одной буквы из строки forbidden.
-    >>> all([avoids("", ""), avoids("a", ""), avoids("", "x")])
-    True
-    >>> avoids("banana", "xyz"), avoids("apple", "p"), avoids("Python", "p")
-    (True, False, True)
-    """
-    return not any({letter in forbidden for letter in word})
+    """Возвращает True, если в слове word нет ни одной буквы из строки forbidden."""
+    return not any(letter in forbidden for letter in word)
+
+
+if __name__ == "__main__":
+    assert all([avoids("", ""), avoids("a", ""), avoids("", "x")])
+    assert (avoids("banana", "xyz"), avoids("apple", "p"), avoids("Python", "p")) == (True, False, True)
+    print("Все тесты прошли успешно!")
