73455314ab5e067fa38da2d56d740d052404b8e7
koru07478-cpu <koru07478@gmail.com>
2026-07-17 15:30:25 +0300

    глава 19 часть 1 в 5 и часть доктесты заменила на pytest

--- chapter19/part02/ex07.py
+++ chapter19/part02/ex07.py
@@ -1,18 +1,27 @@
 from collections import defaultdict
+import pytest
 
 
 def classify_words(words: list[str]) -> dict[str, list[str]]:
     """Функция принимает список слов и возвращает словарь, в котором
-    каждой букве сопоставлен список слов, которые начинаются с этой буквы.
-    >>> classify_words(['яблоко', 'банан', 'ягода', 'апельсин'])
-    {'я': ['яблоко', 'ягода'], 'б': ['банан'], 'а': ['апельсин']}
-    >>> classify_words([])
-    {}
-    >>> classify_words(['кот', '', 'крот'])
-    {'к': ['кот', 'крот']}
-    """
+    каждой букве сопоставлен список слов, которые начинаются с этой буквы."""
     d = defaultdict(list)
     for word in words:
         if word:
             d[word[0]].append(word)
     return dict(d)
+
+
+def test_classify_words():
+    assert classify_words(["яблоко", "банан", "ягода", "апельсин"]) == {
+        "я": ["яблоко", "ягода"],
+        "б": ["банан"],
+        "а": ["апельсин"],
+    }
+    assert classify_words([]) == {}
+    assert classify_words(["кот", "", "крот"]) == {"к": ["кот", "крот"]}
+    print("Все тесты для classify_words успешно пройдены!")
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
