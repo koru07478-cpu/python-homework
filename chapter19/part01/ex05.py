9a1eb51fc587580e312ce647f592af006bc398ea
koru07478-cpu <koru07478@gmail.com>
2026-07-17 15:30:32 +0300

    глава 19 часть 1 в 5 и часть доктесты заменила на pytest

--- chapter19/part01/ex05.py
+++ chapter19/part01/ex05.py
@@ -1,9 +1,16 @@
+import pytest
+
+
 def avoids(word: str, forbidden: str) -> bool:
     """Возвращает True, если в слове word нет ни одной буквы из строки forbidden."""
     return not any(letter in forbidden for letter in word)
 
 
-if __name__ == "__main__":
+def test_count_it():
     assert all([avoids("", ""), avoids("a", ""), avoids("", "x")])
     assert (avoids("banana", "xyz"), avoids("apple", "p"), avoids("Python", "p")) == (True, False, True)
-    print("Все тесты прошли успешно!")
+    print("Все тесты для avoids успешно пройдены!")
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
